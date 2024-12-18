5ccc56029bbef1cba1df9758fe2c07a9a9b82aca
hitzza <cslzh4433@gmail.com>
2024-12-18 23:17:09 +0900

    add login common components

---
+++ src/app/(afterlogin)/common/Footer.tsx
@@ -0,0 +1,6 @@
+
+const login_footer = () => {
+    return <footer></footer>
+}
+
+export default login_footer;
---
+++ src/app/(afterlogin)/common/Header.tsx
@@ -0,0 +1,6 @@
+
+const login_header = () => {
+    return <header></header>
+}
+
+export default login_header;
